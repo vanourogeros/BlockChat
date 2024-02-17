81135ff80ce1cb7af70648dd958370a674ab60c2
Ioannis Protogeros <61976410+vanourogeros@users.noreply.github.com>
2024-02-18 01:35:39 +0200

    Initialize Bootstrap Node (WIP?)

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from wallet import Wallet
+
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOOTSTRAP_IP = os.getenv("BOOTSTRAP_IP") # 127.0.0.1
+BOOTSTRAP_PORT = int(os.getenv("BOOTSTRAP_PORT")) # 5000
+
+app = Flask(__name__)
+
+ip_address = sys.argv[1]
+port = int(sys.argv[2])
+
+bootstrap = ip_address == BOOTSTRAP_IP and port == BOOTSTRAP_PORT
+
+wallet = Wallet(ip_address, port, bootstrap)
+
+@app.route('/api/get_balance')
+def get_balance():
+    return jsonify({"balance": wallet.balance})
+
+if __name__ == '__main__':
+    app.run(host=ip_address, port=port)
--- src/wallet.py
+++ src/wallet.py
@@ -2,27 +2,55 @@ import time
 from block import Block
 from transaction import Transaction
 from blockchain import Blockchain
+import requests
 
 from Crypto.PublicKey import RSA
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+CAPACITY = int(os.getenv("CAPACITY"))
+TOTAL_NODES = int(os.getenv("TOTAL_NODES"))
 
 class Wallet:
 
     def __init__(self, ip_address: str, port: int, bootstrap=False) -> None:
         self.ip_address = ip_address
         self.port = port
+        self.address = f"{ip_address}:{port}"
+        self.bootstrap = bootstrap
+        self.balance = 0
+        self.stake = 0
         self.public_key, self.private_key = self.generate_wallet()
 
         """
-        State will contain a list of the users and their balances
+        State will contain a list of all the nodes' 
+        {ip_address:port, balance, stake} dictionaries
         """
         self.blockchain_state = []
 
-        """
-        Each node will have a list of the stakes of all nodes
-        """
-        self.stakes = []
         self.transactions_pending = []
         self.blockchain = Blockchain()
+
+        if bootstrap:
+            self.blockchain_state.append({"address": self.address, "balance":0, "stake":0})
+            self.create_genesis_block()
+
+    def create_genesis_block(self):
+        #we call the create transaction function (the same as init of transaction)
+        tran = self.create_transaction(sender_address='0',
+                                      receiver_address=self.address, 
+                                      type_of_transaction="coins", 
+                                      amount=1000*TOTAL_NODES,
+                                      message="Genesis block",
+                                      nonce=0)
+        # We don't verify the transaction because it's the genesis block, so we process it directly
+        self.process_transaction(tran)
+        genesis_block = Block(index=0, timestamp=time.time(),
+                              transactions=[tran], validator=0, previous_hash=1)
+        self.blockchain.add_block(genesis_block)
+
 
     @staticmethod
     def generate_wallet() -> tuple:
@@ -30,6 +58,32 @@ class Wallet:
         private_key = rsa_keypair.export_key()
         public_key = rsa_keypair.publickey().export_key()
         return (private_key.decode(), public_key.decode())
+    
+    def process_transaction(self, transaction: Transaction) -> None:
+        # Update the balance of the wallet for a verified transaction
+        if transaction.type_of_transaction == "coins":
+            if transaction.receiver_address == self.address:
+                self.balance += transaction.amount
+            elif transaction.sender_address == self.address:
+                self.balance -= transaction.amount
+        self.add_transaction(transaction)
+        return
+
+    def get_network_state(self):
+        if self.bootstrap:
+            return self.blockchain_state
+        else:
+            #TODO: This is not right lmao
+            response = requests.post(f"http://{self.ip_address}:{self.port}/api/get_network_state")
+            if response.status_code == 200:
+                return response.json()["data"]
+            else:
+                return []
+
+
+    def register_node(self, ip_address: str, port: int) -> None:
+        self.blockchain_state.append((ip_address, port, 0))
+        self.stakes.append(0)
     
     def create_transaction(self, sender_address: str, receiver_address: str, 
                            type_of_transaction: str, amount: float, message: str, nonce: int
@@ -42,8 +96,8 @@ class Wallet:
         self.transactions_pending.append(transaction)
         
     def mine_block(self):
-        if not self.transactions_pending:
-            print("No transactions to mine")
+        if len(self.transactions_pending) == 0:
+            print(f"(pid={os.getpid}) [INVALID BLOCK]: No transactions to mine (pending transactions list is empty)")
             return False
         
         last_block = self.blockchain.chain[-1]
