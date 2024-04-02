5cabdd744db1fdb5c14e12e202af8f5ba35150c3
Panagiotis Xanthopoulos <panos0511@gmail.com>
2024-04-02 17:03:38 +0300

    fix + extend check.py

--- check.py
+++ check.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # ip_dict = {0: '192.168.1.1', 1: '192.168.1.2', 2: '192.168.1.3', 3: '192.168.1.4', 4: '192.168.1.5', 5: '192.168.1.1',
 #            6: '192.168.1.2', 7: '192.168.1.3', 8: '192.168.1.4', 9: '192.168.1.5', 10: '192.168'}
@@ -7,7 +8,31 @@ ip_dict = {0: '127.0.0.1', 1: '127.0.0.1', 2: '127.0.0.1', 3: '127.0.0.1', 4: '1
 port_dict = {0: 5000, 1: 5001, 2: 5002, 3: 5003, 4: 5004}
 
 
-def check_balance():
+def check_balance_hard():
+    totals = []
+    for ip, port in zip(ip_dict.values(), port_dict.values()):
+        total = 0
+        url = f"http://{ip}:{port}/api/get_network_state_hard"
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                for ip_inner, port_inner in zip(ip_dict.values(), port_dict.values()):
+                    total += response.json()[f'{ip_inner}:{port_inner}']['balance']
+                totals.append(total)
+            else:
+                print("Error:", response.status_code)
+        except requests.exceptions.RequestException as e:
+            print("Error:", e)
+
+    if all(value == 50000.0 for value in totals):
+        print("Balances from hard state are all 50000.")
+    elif all(value == totals[0] for value in totals):
+        print(f"Balances from hard state are all the same but NOT 50000 ({totals[0]}).")
+    else:
+        print(f"Balances from hard state are NOT good: {totals}.")
+
+
+def check_balance_soft():
     totals = []
     for ip, port in zip(ip_dict.values(), port_dict.values()):
         total = 0
@@ -23,12 +48,11 @@ def check_balance():
         except requests.exceptions.RequestException as e:
             print("Error:", e)
 
-    if all(value == 50000.0 for value in totals):
-        print("Balances are all 50000")
-    elif all(value == totals[0] for value in totals):
-        print(f"Balances are all the same but not 50000 ({totals[0]})")
+    if all(value == totals[0] for value in totals):
+        print(f"Balances from soft state are all the same.")
     else:
-        print(f"Balances are not good: {totals}")
+        print(f"Balances from soft state are NOT all the same: {totals}.")
+    return totals
 
 
 def check_pending():
@@ -54,10 +78,35 @@ def check_pending():
         print(f"All pending_transaction lists have the same transactions, but there are {len(pending_sets[0])}"
               f" transactions.")
     elif not len(set(frozen_sets)) == 1 and all(len(s) == 4 for s in pending_sets):
-        print("All pending_transaction lists have 4 transactions, but they are not the same.")
+        print("All pending_transaction lists have 4 transactions, but they are NOT the same.")
     else:
         print("Shit is bad.")
 
 
-check_balance()
+def calc_reward_from_pending(totals):
+    rewards = []
+    for ip, port in zip(ip_dict.values(), port_dict.values()):
+        reward = 0
+        url = f"http://{ip}:{port}/api/pending_transactions"
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                for tran in response.json():
+                    reward += len(json.loads(tran)['message'])
+                rewards.append(reward)
+            else:
+                print("Error:", response.status_code)
+        except requests.exceptions.RequestException as e:
+            print("Error:", e)
+
+    new_totals = [x + y for x, y in zip(totals, rewards)]
+    if all(value == 50000.0 for value in new_totals):
+        print("Balances from soft state are 50000 - reward from pending.")
+    else:
+        print("Balances from soft state are NOT 50000 - reward from pending.")
+
+
+check_balance_hard()
+soft_totals = check_balance_soft()
 check_pending()
+calc_reward_from_pending(soft_totals)
